c690473e64d47e7d7b70b48c077badd8858fa6d7
RulyVR <141476783+Rulyvazquez@users.noreply.github.com>
2024-12-28 14:57:28 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 # setup.py
 private_keys = [
-    'YOUR PRIVATE KEY WALLET 1'
+    'cb23f56dbb3e2f2b342ac2f79c1cc03ff131766ac0c302c22edc957e1d267635'
 ]
 
 addresses = [
-    'ADDRESS 1'  # Alamat address EVM / Your EVM Address
+    '0x83776b981d808b479b0b20a95f8b76c2aea4312a'  # Alamat address EVM / Your EVM Address
 ]
 
 labels = [
-    'Wallet 1'
+    'Rulyvr'
 ]
