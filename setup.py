77893d1c351b9c983859ee81088251c4f56e7e8d
RulyVR <141476783+Rulyvazquez@users.noreply.github.com>
2024-12-29 13:58:04 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 # setup.py
 private_keys = [
-    'cb23f56dbb3e2f2b342ac2f79c1cc03ff131766ac0c302c22edc957e1d267635'
+    ''
 ]
 
 addresses = [
-    '0x83776b981d808b479b0b20a95f8b76c2aea4312a'  # Alamat address EVM / Your EVM Address
+    ''  # Alamat address EVM / Your EVM Address
 ]
 
 labels = [
